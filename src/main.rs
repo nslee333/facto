bc454ffb3406446393d5755b126f9717cf2b3a45
Nathan <nslee333@gmail.com>
2025-03-21 15:47:42 -0700

    building import data to db functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::vec::Vec;
+use sqlite;
 
-
+// TODO add cli interface 
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
     let file_result = File::open("questions.txt");
 
     let file = match file_result {
@@ -14,9 +15,32 @@ fn main() -> io::Result<()> {
 
     let reader = io::BufReader::new(file);
 
+    let mut vec = Vec::new();
+
+    
     for line in reader.lines() {
-        println!("{}", line?);
+        vec.push(line?);
     }
+
+    Ok(())
+
+}
+
+// fn read_file(file: String) -> Result<Vec<String>, Err> {
+
+
+
+// }
+
+fn import_questions_to_db() -> Result<()> {
+
+    let connection = sqlite::open(":memory:").unwrap();
+
+    let query = "
+        CREATE TABLE questions (id INTEGER primary key autoincrement, question TEXT);
+        INSERT INTO questions VALUES () 
+    ";
+    // TODO finish up insert, extract this code into library methods
 
     Ok(())
 }
