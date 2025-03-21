5af1c9d202f8c46c29a07d787292970cb352cd7d
Nathan <nslee333@gmail.com>
2025-03-21 15:20:47 -0700

    read a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use std::fs::File;
+use std::io::{self, BufRead};
+
+
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+    let file_result = File::open("questions.txt");
+
+    let file = match file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem with opening file: {error:?}"),
+    };
+
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
